f0ce7a094acd51dea8873a0592b320f141412b2c
Dmitry Belyaev <be.dmitry@gmail.com>
2026-01-02 23:32:47 +0000

    Fix Elixir 1.19 warnings

--- test/elixir/support/fixtures/compiler/transformer/module_109.ex
+++ test/elixir/support/fixtures/compiler/transformer/module_109.ex
@@ -4,7 +4,7 @@ defmodule Hologram.Test.Fixtures.Compiler.Tranformer.Module109 do
 
   defstruct [:a, :b]
 
-  def test(x) do
+  def test(%__MODULE__{} = x) do
     %Module109{x | a: 1, b: 2}
   end
 end
